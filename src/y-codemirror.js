8afecc47c439dee76cd74cd3694549b3c8f5fe6c
Kevin Jahns <kevin.jahns@protonmail.com>
2020-12-28 18:07:01 +0100

    add theming for ycursors

--- src/y-codemirror.js
+++ src/y-codemirror.js
@@ -1,10 +1,70 @@
 
 import * as Y from 'yjs' // eslint-disable-line
 import { StateField, Facet, ChangeSet, Transaction, Annotation, AnnotationType } from '@codemirror/next/state' // eslint-disable-line
-import { Range, ViewPlugin, PluginValue, ViewUpdate, EditorView, Decoration } from '@codemirror/next/view' // eslint-disable-line
+import { Range, ViewPlugin, PluginValue, ViewUpdate, EditorView, Decoration, themeClass } from '@codemirror/next/view' // eslint-disable-line
 import { RangeSet, RangeSetBuilder } from '@codemirror/next/rangeset'
 import * as dom from 'lib0/dom.js'
 import * as pair from 'lib0/pair.js'
+
+const baseTheme = EditorView.baseTheme({
+  $yselection: {
+  },
+  $ycursor: {
+    position: 'relative'
+  },
+  $ycursorInfo: {
+    position: 'absolute',
+    top: '-1.05em',
+    left: '-1px',
+    fontSize: '.6em',
+    fontFamily: 'serif',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    lineHeight: 'normal',
+    userSelect: 'none',
+    color: 'white',
+    paddingLeft: '2px',
+    paddingRight: '2px',
+    zIndex: 3,
+    transition: 'opacity .3s ease-in-out'
+  }
+})
+
+/*
+.remote-caret {
+  position: relative;
+  border-left: 1px solid black;
+  border-right: 1px solid black;
+  margin-left: -1px;
+  margin-right: -1px;
+  box-sizing: border-box;
+}
+.remote-caret > div {
+  position: absolute;
+  top: -1.05em;
+  left: -1px;
+  font-size: .6em;
+  background-color: rgb(250, 129, 0);
+  font-family: serif;
+  font-style: normal;
+  font-weight: normal;
+  line-height: normal;
+  user-select: none;
+  color: white;
+  padding-left: 2px;
+  padding-right: 2px;
+  z-index: 3;
+  transition: opacity .3s ease-in-out;
+}
+.remote-caret.hide-name > div {
+  transition-delay: .7s;
+  opacity: 0;
+}
+.remote-caret:hover > div {
+  opacity: 1;
+  transition-delay: 0s;
+}
+*/
 
 /**
  * @type {AnnotationType<YCollabConfig>}
@@ -75,7 +135,7 @@ class YRemoteCursorWidget {
   }
 
   toDOM () {
-    return /** @type {HTMLElement} */ (dom.element('span', [pair.create('class', 'y-cm-cursor'), pair.create('style', `color: ${this.color}`)]))
+    return /** @type {HTMLElement} */ (dom.element('div', [pair.create('class', themeClass('ycursor')), pair.create('style', `background-color: ${this.color}`)]))
   }
 
   eq (widget) {
@@ -116,13 +176,13 @@ class YCollabCursorViewPluginValue {
   update (update) {
     const decorations = new RangeSetBuilder()
     decorations.add(1, 5, Decoration.mark({
-      attributes: { color: 'blue' },
-      class: 'y-cm-selection'
+      attributes: { style: 'background-color: orange' },
+      class: themeClass('yselection')
     }))
     decorations.add(5, 5, Decoration.widget({
       side: 5 - 1 > 0 ? -1 : 1,
       block: false,
-      widget: new YRemoteCursorWidget('blue')
+      widget: new YRemoteCursorWidget('orange')
     }))
     this.decorations = decorations.finish()
   }
@@ -154,6 +214,7 @@ export const yCollabConfig = Facet.define({
  * @param {any} awareness
  */
 export const ycollab = (ytext, awareness) => [
+  baseTheme,
   yCollabConfig.of(new YCollabConfig(ytext, awareness)),
   YCollabViewPlugin,
   YCollabCursorPlugin
